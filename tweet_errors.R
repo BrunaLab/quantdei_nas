90ce4952d6d72102656fae8bf47e53a5e2ded453
embruna <embruna@ufl.edu>
2023-02-22 15:08:34 -0500

    changing script titles

--- tweet_errors.R
+++ tweet_errors.R
@@ -62,10 +62,17 @@ race_fail <- race_tweets %>%
         str_detect(text,"amazing race")|
         str_detect(text,"boston")
       )==TRUE) %>% 
-  filter(str_detect(text,"equity")==FALSE) %>% 
-  filter(str_detect(text,"racism")==FALSE) %>% 
-  filter(str_detect(text,"diversity")==FALSE) %>% 
-  mutate(fail_category="race")
+  filter(str_detect(text,"gender")==FALSE) %>% 
+  filter(str_detect(text,"inclusion")==FALSE) %>%
+  filter(str_detect(text,"racism")==FALSE) %>%
+  filter(str_detect(text,"racial")==FALSE) %>%
+  filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>%
+  filter(str_detect(text,"reform")==FALSE) %>%
+  filter(str_detect(text,"equality")==FALSE) %>%
+  filter(str_detect(text,"oppression")==FALSE) %>%
+  filter(str_detect(text,"social")==FALSE) %>% 
+mutate(fail_category="race")
 
 (nrow(race_fail)/nrow(race_tweets))*100
 
@@ -82,19 +89,20 @@ justice_fail <- justice_tweets %>%
   filter(
     (
       str_detect(text,"supreme court")|
-        str_detect(text,"sandra day o¡¯connor")|
+        str_detect(text,"connor")|
         str_detect(text,"online")|
         str_detect(text,"commencement")|
         str_detect(text,"criminal justice")
     )==TRUE) %>% 
-  filter(str_detect(text,"equity")==FALSE) %>% 
-  filter(str_detect(text,"race")==FALSE) %>% 
-  filter(str_detect(text,"racism")==FALSE) %>% 
-  filter(str_detect(text,"racial")==FALSE) %>% 
-  filter(str_detect(text,"diversity")==FALSE) %>%
-  filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>% 
   filter(str_detect(text,"inclusion")==FALSE) %>%
+  filter(str_detect(text,"racism")==FALSE) %>%
+  filter(str_detect(text,"racial")==FALSE) %>%
+  filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>%
   filter(str_detect(text,"reform")==FALSE) %>%
+  filter(str_detect(text,"equality")==FALSE) %>%
+  filter(str_detect(text,"oppression")==FALSE) %>%
   filter(str_detect(text,"social")==FALSE) %>% 
   mutate(fail_category="justice")
 
@@ -123,13 +131,15 @@ diversity_fail <- diversity_tweets %>%
         str_detect(text,"biodiversity")|
         str_detect(text,"plant") 
     )==TRUE) %>% 
-  filter(str_detect(text,"equity")==FALSE) %>% 
-  filter(str_detect(text,"race")==FALSE) %>% 
-  filter(str_detect(text,"inclusion")==FALSE) %>% 
-  filter(str_detect(text,"racism")==FALSE) %>% 
+  filter(str_detect(text,"gender")==FALSE) %>% 
+  filter(str_detect(text,"inclusion")==FALSE) %>%
+  filter(str_detect(text,"racism")==FALSE) %>%
   filter(str_detect(text,"racial")==FALSE) %>%
   filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>%
   filter(str_detect(text,"reform")==FALSE) %>%
+  filter(str_detect(text,"equality")==FALSE) %>%
+  filter(str_detect(text,"oppression")==FALSE) %>%
   filter(str_detect(text,"social")==FALSE) %>% 
   mutate(fail_category="diversity")
   
@@ -145,24 +155,131 @@ equity_tweets <- t %>%
   mutate(text=tolower(text)) %>% 
   filter(str_detect(text,"equity")==TRUE) 
 
-
-
-# equity_fail <- equity_tweets %>% filter(str_detect(text,"equity")==TRUE)
-
 equity_fail <- equity_tweets %>% 
   filter(
     (
       str_detect(text,"private equity")
     )==TRUE) %>% 
-  filter(str_detect(text,"race")==FALSE) %>% 
-  filter(str_detect(text,"inclusion")==FALSE) %>% 
-  filter(str_detect(text,"racism")==FALSE) %>% 
+  filter(str_detect(text,"gender")==FALSE) %>% 
+  filter(str_detect(text,"inclusion")==FALSE) %>%
+  filter(str_detect(text,"racism")==FALSE) %>%
   filter(str_detect(text,"racial")==FALSE) %>%
   filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>%
   filter(str_detect(text,"reform")==FALSE) %>%
+  filter(str_detect(text,"equality")==FALSE) %>%
+  filter(str_detect(text,"oppression")==FALSE) %>%
   filter(str_detect(text,"social")==FALSE) %>% 
   mutate(fail_category="equity")
 
+
+
+
+# advocacy errors ---------------------------------------------------------
+
+
+
+advocacy_tweets <- t %>% 
+  mutate(text=tolower(text)) %>% 
+  filter(str_detect(text,"advocacy")==TRUE) 
+
+advocacy_fail <- advocacy_tweets %>% 
+  filter(
+    (
+      str_detect(text,"patient advocacy")|
+        str_detect(text,"law school")|
+        str_detect(text,"appellate")|
+        str_detect(text,"truman")| #truman scholarship winners
+        str_detect(text,"trial")|
+        str_detect(text,"veterans")|
+        str_detect(text,"legislature")
+    )==TRUE) %>% 
+  filter(str_detect(text,"gender")==FALSE) %>% 
+  filter(str_detect(text,"inclusion")==FALSE) %>%
+  filter(str_detect(text,"racism")==FALSE) %>%
+  filter(str_detect(text,"racial")==FALSE) %>%
+  filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>%
+  filter(str_detect(text,"reform")==FALSE) %>%
+  filter(str_detect(text,"equality")==FALSE) %>%
+  filter(str_detect(text,"oppression")==FALSE) %>%
+  filter(str_detect(text,"social")==FALSE) %>% 
+  mutate(fail_category="advocacy")
+
+
+
+
+
+# ally errors -------------------------------------------------------------
+
+
+ally_tweets <- t %>% 
+  mutate(text=tolower(text)) %>% 
+  filter(str_detect(text,"ally")==TRUE) 
+
+ally_fail <- ally_tweets %>% 
+  filter(
+    (
+      str_detect(text,"@ally_")|
+        str_detect(text,"@ally")|
+        str_detect(text,"softball")|
+        str_detect(text,"tennis")|
+        str_detect(text,"gymnast")|
+        str_detect(text,"lacross")|
+        str_detect(text,"water polo")|
+        str_detect(text,"waterpolo")|
+        str_detect(text,"basketball")|
+        str_detect(text,"soccer")|
+        str_detect(text,"single"))==TRUE) %>% 
+  filter(str_detect(text,"gender")==FALSE) %>% 
+  filter(str_detect(text,"inclusion")==FALSE) %>%
+  filter(str_detect(text,"racism")==FALSE) %>%
+  filter(str_detect(text,"racial")==FALSE) %>%
+  filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>%
+  filter(str_detect(text,"reform")==FALSE) %>%
+  filter(str_detect(text,"equality")==FALSE) %>%
+  filter(str_detect(text,"oppression")==FALSE) %>%
+  filter(str_detect(text,"social")==FALSE) %>% 
+  mutate(fail_category="ally")
+
+
+
+
+
+
+
+# privilege errors -------------------------------------------------------------
+
+
+privilege_tweets <- t %>% 
+  mutate(text=tolower(text)) %>% 
+  filter(str_detect(text,"privilege")==TRUE) 
+
+privilege_fail <- privilege_tweets %>% 
+  filter(
+    (
+      str_detect(text,"a privilege to") |
+        str_detect(text,"my privilege to") |
+  str_detect(text,"the privilege to ") |
+    str_detect(text,"privilege to ") |
+    str_detect(text,"privilege and honor ") |
+    str_detect(text,"privilege and an ") |
+    str_detect(text,"privilege of ") |
+  str_detect(text,"the privilege of "))==TRUE) %>% 
+  filter(str_detect(text,"gender")==FALSE) %>% 
+  filter(str_detect(text,"inclusion")==FALSE) %>%
+  filter(str_detect(text,"racism")==FALSE) %>%
+  filter(str_detect(text,"racial")==FALSE) %>%
+  filter(str_detect(text,"ally")==FALSE) %>%
+  filter(str_detect(text,"your privilege")==FALSE) %>%
+  filter(str_detect(text,"civil right")==FALSE) %>%
+  filter(str_detect(text,"gender")==FALSE) %>%
+  filter(str_detect(text,"reform")==FALSE) %>%
+  filter(str_detect(text,"equality")==FALSE) %>%
+  filter(str_detect(text,"oppression")==FALSE) %>%
+  filter(str_detect(text,"social")==FALSE) %>% 
+  mutate(fail_category="privilege")
 
 
 
@@ -173,7 +290,9 @@ equity_fail <- equity_tweets %>%
 tweet_fail<-bind_rows(race_fail,
                        equity_fail,
                        diversity_fail,
-                       justice_fail) %>% 
+                       justice_fail,
+                      advocacy_fail,
+                      ally_fail) %>% 
   relocate(fail_category,.before=1)
 
 overall_fail_perc<-(nrow(tweet_fail)/nrow(t))*100
@@ -186,3 +305,27 @@ justice_fail_perc<-(nrow(justice_fail)/nrow(justice_tweets))*100
 justice_fail_perc
 equity_fail_perc<-(nrow(equity_fail)/nrow(equity_tweets))*100
 equity_fail_perc
+advocacy_fail_perc<-(nrow(advocacy_fail)/nrow(advocacy_tweets))*100
+advocacy_fail_perc
+ally_fail_perc<-(nrow(ally_fail)/nrow(ally_tweets))*100
+ally_fail_perc
+privilege_fail_perc<-(nrow(privilege_fail)/nrow(privilege_tweets))*100
+privilege_fail_perc
+
+
+
+
+# plots -------------------------------------------------------------------
+priv_plot<-privilege_tweets %>% 
+  separate(created_at, c("year","month","remainder")) %>% 
+  select(-remainder) %>% 
+  group_by(month,year) %>% 
+  summarize(n=n()) %>% 
+  mutate(year=as.integer(year)) %>% 
+  mutate(date=paste(year,month,sep="-")) %>% 
+  filter(year>2015)
+
+
+ggplot(data=priv_plot, aes(x=date, y=n, group=1)) +
+  geom_line()+
+  theme_classic()
